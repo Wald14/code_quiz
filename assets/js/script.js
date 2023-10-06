0d71861be7d40558ae79ec99b5e832735839ce89
Wald14 <lukewald@hotmail.com>
2023-10-05 22:49:19 -0500

    Added quiz timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+// Element Queries
 var startBoxEl = document.querySelector("#startBox");
 var questionBoxEl = document.querySelector("#question-box");
 var userInputBoxEl = document.querySelector("#user-input-box");
-var highScoreBoxEl = document.querySelector("high-score-box");
+var highScoreBoxEl = document.querySelector("#high-score-box");
 var startBtnEl = document.querySelector("#start-button");
+var timerCountEl = document.querySelector("#time-count");
+
+
+// Global Variables
+var timerInterval;
+var timer = 75;
 
 // Questions (array of objects)
 var questions = [
@@ -27,6 +34,28 @@ function startGame() {
   startBoxEl.classList.add("hide");
   questionBoxEl.classList.remove("hide");
 
+  countDownTimer();
 }
+
+
+function countDownTimer() {
+  timerInterval = setInterval(function () {
+    // Subtract 1 from timer
+    timer--;
+    // Display time on screen
+    timerCountEl.textContent = timer;
+
+    // End Game if timer hits zero OR goes below zero because the player gets a 10 second penalty with less then 10 seconds left, causing timer to go below 0.
+    if (timer <= 0) {
+      endGame();
+    }
+  }, 1000)
+}
+
+function endGame(){
+
+}
+
+
 
 startBtnEl.addEventListener("click", startGame);
